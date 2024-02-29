8d168316ef22ad5b009ab330172a198409602e07
Annie Pennell <anniecpennell@gmail.com>
2024-02-28 16:17:18 -0800

    Remove .next-site directory

--- .next-site/sanity/config/client-config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  projectId: "g31vrzho",
-  dataset: "production",
-  apiVersion: "2023-04-11",
-  useCdn: true,
-};
-
-export default config;
--- .next-site/sanity/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-import page from "./page-schema";
-import project from "./project-schema";
-
-const schemas = [page, project];
-
-export default schemas;
